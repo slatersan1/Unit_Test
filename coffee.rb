d633d4e114e23bc98554aebfa29f44c3f6e233f9
slatersan1 <cdlee212@gmail.com>
2016-03-01 21:53:28 -0500

    decent solution

--- coffee.rb
+++ coffee.rb
@@ -1,5 +1,30 @@
 class Coffee
+	
+	def initialize(coffee)
+		@coffee = 9
+	end	
+	
 	def full?
-		true
+		@coffee == 9
+
+		# if @coffee < 9 && @coffee > 0
+		# 	false
+		# else
+		# 	true
+		# end	
 	end
+
+	def empty?
+		@coffee == 0
+
+		# if @coffee >= 9 && @coffee < 1
+		# 	false
+		# else
+		# 	true
+		# end		
+	end
+
+	def sip
+		@coffee -= 3
+	end	
 end		
--- tests.rb
+++ tests.rb
@@ -3,7 +3,9 @@ require 'minitest/autorun'
 require './human'
 require './coffee'
 
+
 class CaffeineTest < MiniTest::Test
+  
   def test_humans_tend_to_be_sleepy
     dave = Human.new "Dave"
     assert dave.alertness < 0.1
@@ -22,7 +24,7 @@ class CaffeineTest < MiniTest::Test
 
     mallory.buy tsmf
     mallory.drink!
-    assert_within_epsilon mallory.alertness, 0.33, 0.1
+    assert_in_epsilon mallory.alertness, 0.33, 0.1
     refute tsmf.full?
     refute tsmf.empty?
   end
